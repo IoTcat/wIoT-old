321e48d894b5509f7e0440cafe4a470be5969e24
IoTcat <i@iotcat.me>
2019-05-20 13:57:17 +0800

    change docs

---
+++ docs/config.js
@@ -0,0 +1,71 @@
+const langs = [
+    { title: 'English', path: '/home', matchPath: /^\/(home|ecosystem|support)/ },
+    { title: '简体中文', path: '/zh-Hans/', matchPath: /^\/zh-Hans/ },
+];
+
+docute.init({
+    landing: 'landing.html',
+    title: 'wIoT',
+    repo: 'iotcat/wIoT',
+    twitter: 'wiott',
+    'edit-link': 'https://github.com/iotcat/wiot/docs',
+    nav: {
+        default: [
+            {
+                title: 'Home', path: '/home'
+            },
+            {
+                title: 'Ecosystem', path: '/ecosystem'
+            },
+            {
+                title: 'Support DPlayer', path: '/support'
+            },
+            {
+                title: 'Languages', type: 'dropdown', items: langs
+            }
+        ],
+        'zh-Hans': [
+            {
+                title: '首页', path: '/zh-Hans/'
+            },
+            {
+                title: '生态', path: '/zh-Hans/ecosystem'
+            },
+            {
+                title: '支持 DPlayer', path: '/zh-Hans/support'
+            },
+            {
+                title: '选择语言', type: 'dropdown', items: langs
+            }
+        ],
+    },
+    plugins: [
+        docsearch({
+            apiKey: '46a8ba306e368972c1d75feb581fb430',
+            indexName: 'wiot',
+            tags: ['english', 'zh-Hans'],
+            url: 'http://wiot.eee.dog'
+        }),
+        evanyou()
+    ]
+});
+
+function loadScript (src, callback) {
+    let s,
+        r,
+        t;
+    r = false;
+    s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = src;
+    s.onload = s.onreadystatechange = function () {
+    // console.log( this.readyState ); //uncomment this line to see which ready states are called.
+        if (!r && (!this.readyState || this.readyState == 'complete'))
+        {
+            r = true;
+            callback();
+        }
+    };
+    t = document.getElementsByTagName('script')[0];
+    t.parentNode.insertBefore(s, t);
+}
