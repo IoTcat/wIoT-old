09c1c1e8cd95ebd39dc7f1a62b163ea003b6b05a
iotcat <i@iotcat.me>
2020-01-07 11:26:21 +0000

    fix selector

--- docs/config.js
+++ docs/config.js
@@ -41,7 +41,7 @@ docute.init({
     },
     plugins: [
         docsearch({
-            apiKey: '4de49b1faa7fecd36ac7a295fcb55229',
+            apiKey: '4de49b1faa7fecd36ac7a295fcb552296',
             indexName: 'wiot',
             tags: ['english', 'zh-Hans'],
             url: 'http://wiot.eee.dog'
